59adf2b1a6ce80c60dfc75abc863e8f4fea5b977
Dusan Sarac <duka.sarac@gmail.com>
2022-11-19 15:53:00 +0100

    Increment and Decrement

---
+++ incrementcount/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CounterHooks from './CounterHooks'
+
+function App() {
+  return (
+    <>  
+     
+    <CounterHooks initialCount = {0}/>
+    CounterHooks
+
+    
+    
+    </>
+  );
+}
+
+export default App;
---
+++ incrementcount/src/CounterHooks.js
@@ -0,0 +1,25 @@
+import React   from "react";
+import { useState } from "react";
+
+
+
+export default function CounterHooks({initialCount}){
+const [state, setState] = useState({ count : initialCount})
+return (
+    <div>
+       <button onClick={() => setState({count: state.count - 1})}>-</button>
+       <span> {state.count}</span>   
+       <button onClick={() => setState({count: state.count + 1})}>+</button> 
+
+
+
+    </div>
+
+
+
+)
+
+
+
+
+}
